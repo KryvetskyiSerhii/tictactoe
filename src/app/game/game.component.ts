f2bace5b46320940cecd722c647f87ed6f35c609
Kryvetskyi Serhii <sam.hartman@mail.ru>
2022-09-23 22:19:25 +0200

    done

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -14,12 +14,37 @@ export class GameComponent implements OnInit {
 
   startGame(): void {
     this.game.gameStart();
+    const currentPlayer = `Current turn: Player ${this.game.currentTurn}`;
+    const turnInformation = document.querySelector('.current-turn');
+    if (turnInformation !== null) turnInformation.innerHTML = currentPlayer;
   }
 
   async clickSubField(subfield: any): Promise<void> {
     if (this.game.gameStatus === 1) {
       const position = subfield.currentTarget.getAttribute('position');
-      console.log(position);
+      const turnInformation = document.querySelector('.current-turn');
+      this.game.setField(position, this.game.currentTurn);
+      const image = this.game.getPlayerImage();
+      subfield.currentTarget.innerHTML = `<img src="${image}" class="item__img" alt=""  />`;
+      this.game.changeCurrentTurn();
+
+      await this.game.checkGameWinner().then((end: boolean) => {
+        if (this.game.gameStatus === 0 && end) {
+          if (turnInformation !== null)
+            turnInformation.innerHTML = `The winner is Player ${this.game.currentTurn}`;
+        }
+      });
+
+      await this.game.checkGameIsOVer().then((end: boolean) => {
+        if (this.game.gameStatus === 0 && end) {
+          if (turnInformation !== null)
+            turnInformation.innerHTML = 'No winner, draw';
+        }
+      });
+      if (this.game.gameStatus === 1) {
+        const currentPlayer = `Current turn: Player ${this.game.currentTurn}`;
+        if (turnInformation !== null) turnInformation.innerHTML = currentPlayer;
+      }
     }
   }
 }
--- src/app/gamelogic.ts
+++ src/app/gamelogic.ts
@@ -7,6 +7,27 @@ export class Gamelogic {
 
   gameStatus: Status;
 
+  winSituationPlayerOne: Array<Array<number>> = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+  ];
+  winSituationPlayerTwo: Array<Array<number>> = [
+    [2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 2, 2],
+    [2, 0, 0, 2, 0, 0, 2, 0, 0],
+    [0, 2, 0, 0, 2, 0, 0, 2, 0],
+    [0, 0, 2, 0, 0, 2, 0, 0, 2],
+    [2, 0, 0, 0, 2, 0, 0, 0, 2],
+    [0, 0, 2, 0, 2, 0, 2, 0, 0],
+  ];
+
   public constructor() {
     this.gameStatus = Status.STOP;
     this.gamefield = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -16,12 +37,71 @@ export class Gamelogic {
   gameStart(): void {
     this.gamefield = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.currentTurn = this.randomPlayerStart();
-    console.log(this.currentTurn);
-
     this.gameStatus = Status.START;
   }
 
   randomPlayerStart(): any {
     return Math.floor(Math.random() * 2) + 1;
+  }
+
+  setField(position: number, value: number): void {
+    this.gamefield[position] = value;
+  }
+
+  getPlayerImage(): any {
+    if (this.currentTurn === 1) return '../../assets/cross.svg';
+    else return '../../assets/zero.svg';
+  }
+
+  changeCurrentTurn(): void {
+    this.currentTurn = this.currentTurn === 1 ? 2 : 1;
+  }
+
+  arrayEquals(a: Array<any>, b: Array<any>): boolean {
+    let isEqual = 0;
+    if (a.length === b.length && Array.isArray(a) && Array.isArray(b)) {
+      b.forEach((item, index) => {
+        if (item === 0 && item !== a[index]) isEqual += 1;
+      });
+    }
+    return isEqual > 0 ? false : true;
+  }
+
+  async checkGameWinner(): Promise<boolean> {
+    let isWinner = false;
+    const checkTheArray =
+      this.currentTurn === 1
+        ? this.winSituationPlayerOne
+        : this.winSituationPlayerTwo;
+    let currentArray: any[] = [];
+    this.gamefield.forEach((el, index) => {
+      if (el !== this.currentTurn) currentArray[index] = 0;
+      else currentArray[index] = el;
+    });
+
+    checkTheArray.forEach((field) => {
+      if (this.arrayEquals(field, currentArray)) {
+        isWinner = true;
+      }
+    });
+
+    if (isWinner) {
+      this.gameEnd();
+      return true;
+    } else return false;
+  }
+
+  async checkGameIsOVer(): Promise<boolean> {
+    let isFull = true;
+    if (this.gamefield.includes(0)) isFull = false;
+
+    if (isFull) {
+      this.gameEnd();
+      return true;
+    } else return false;
+  }
+
+  gameEnd(): void {
+    this.gameStatus = Status.STOP;
   }
 }
